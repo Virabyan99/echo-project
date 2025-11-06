84f58094a5a82c9a617a818534c79e2169d5e661
Virabyan99 <gmparstone99@gmail.com>
2025-11-06 20:02:38 +0400

    03: Clerk Authentication

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+
+    return (
+        <div className="min-h-screen min-w-screen h-full flex flex-col items-center justify-center bg-amber-300">
+            {children}
+        </div>
+    )
+}
+
+export default Layout

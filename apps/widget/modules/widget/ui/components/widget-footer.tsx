beaab5d449a72ff1d59fa18d71c241156251bf33
Virabyan99 <gmparstone99@gmail.com>
2025-11-12 15:28:08 +0400

    10: widget Session

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -21,7 +21,7 @@ export const WidgetFooter = () => {
         size="icon"
         variant="ghost">
         <InboxIcon
-          className={cn('size-5', screen === 'inbox' && 'text-primary')}
+          className={cn('size-5')}
         />
       </Button>
     </footer>

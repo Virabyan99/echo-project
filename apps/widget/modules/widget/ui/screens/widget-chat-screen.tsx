4d0fdb8d04ef2b9f7b5194beee98ca2a3d5c1cb1
Virabyan99 <gmparstone99@gmail.com>
2025-11-13 21:15:18 +0400

    14: AI agents

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { useThreadMessages, toUIMessages } from '@convex-dev/agent/react'
 import { Button } from '@workspace/ui/components/button'
 import { WidgetHeader } from '../components/widget-header'
 import { ArrowLeft, MenuIcon } from 'lucide-react'
@@ -10,11 +14,36 @@ import {
   organizationIdAtom,
   screenAtom,
 } from '../../atoms/widget-atoms'
-import { useQuery } from 'convex/react'
+import { useAction, useQuery } from 'convex/react'
 import { api } from '@workspace/backend/_generated/api'
+import {
+  AIConversation,
+  AIConversationContent,
+  AIConversationScrollButton,
+} from '@workspace/ui/components/ai/conversation'
+import {
+  AIInput,
+  AIInputSubmit,
+  AIInputTextarea,
+  AIInputToolbar,
+  AIInputTools,
+} from '@workspace/ui/components/ai/input'
+import {
+  AIMessageContent,
+  AIMessage,
+} from '@workspace/ui/components/ai/message'
+import { AIResponse } from '@workspace/ui/components/ai/response'
+import {
+  AISuggestion,
+  AISuggestions,
+} from '@workspace/ui/components/ai/suggestion'
+import { Form, FormField } from '@workspace/ui/components/form'
+
+const formSchema = z.object({
+  message: z.string().min(1, 'Message is required'),
+})
 
 export const WidgetChatScreen = () => {
-
   const setScreen = useSetAtom(screenAtom)
   const setConversationId = useSetAtom(conversationIdAtom)
 
@@ -23,6 +52,11 @@ export const WidgetChatScreen = () => {
   const contactSessionId = useAtomValue(
     contactSessionIdAtomFamily(organizationId || '')
   )
+
+  const onBack = () => {
+    setConversationId(null)
+    setScreen('selection')
+  }
 
   const conversation = useQuery(
     api.public.conversations.getOne,
@@ -34,9 +68,37 @@ export const WidgetChatScreen = () => {
       : 'skip'
   )
 
-  const onBack = () => {
-    setConversationId(null)
-    setScreen('selection')
+  const messages = useThreadMessages(
+    api.public.messages.getMany,
+    conversation?.threadId && contactSessionId
+      ? {
+          threadId: conversation.threadId,
+          contactSessionId,
+        }
+      : 'skip',
+    { initialNumItems: 10 }
+  )
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: '',
+    },
+  })
+
+  const createMessage = useAction(api.public.messages.create)
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (!conversation || !contactSessionId) {
+      return
+    }
+
+    form.reset()
+
+    await createMessage({
+      threadId: conversation.threadId,
+      prompt: values.message,
+      contactSessionId,
+    })
   }
 
   return (
@@ -52,9 +114,70 @@ export const WidgetChatScreen = () => {
           <MenuIcon />
         </Button>
       </WidgetHeader>
-      <div className="flex flex-1 flex-col gap-y-4 p-4">
-        {JSON.stringify(conversation)}
-      </div>
+      <AIConversation>
+        <AIConversationContent>
+          {toUIMessages(messages.results ?? [])?.map((message) => {
+            // Extract text content from message parts
+            const textContent =
+              message.parts
+                ?.filter((part) => part.type === 'text')
+                .map((part) => part.text)
+                .join('') || ''
+
+            return (
+              <AIMessage
+                from={message.role === 'user' ? 'user' : 'assistant'}
+                key={message.id}>
+                <AIMessageContent>
+                  <AIResponse>{textContent}</AIResponse>
+                </AIMessageContent>
+                {/* TODO: add avatar */}
+              </AIMessage>
+            )
+          })}
+        </AIConversationContent>
+      </AIConversation>
+      {/* TODO ADD Suggestions */}
+
+      <Form {...form}>
+        <AIInput
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="rounded-none border-x-0 border-b-0">
+          <FormField
+            control={form.control}
+            disabled={conversation?.status === 'resolved'}
+            name="message"
+            render={({ field }) => (
+              <AIInputTextarea
+                disabled={conversation?.status === 'resolved'}
+                onChange={field.onChange}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault()
+                    form.handleSubmit(onSubmit)()
+                  }
+                }}
+                placeholder={
+                  conversation?.status === 'resolved'
+                    ? 'This Conversation has been resolved'
+                    : 'Type your message...'
+                }
+                value={field.value}
+              />
+            )}
+          />
+          <AIInputToolbar>
+            <AIInputTools />
+            <AIInputSubmit
+              disabled={
+                conversation?.status === 'resolved' || !form.formState.isValid
+              }
+              status='ready'
+              type='submit'
+            />
+          </AIInputToolbar>
+        </AIInput>
+      </Form>
     </>
   )
 }
--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,6 +1,8 @@
 import { ConvexError, v } from 'convex/values'
 import { mutation, query } from '../_generated/server'
-import { threadId } from 'worker_threads'
+import { supportAgent } from '../system/ai/agents/supportAgent'
+import { saveMessage } from '@convex-dev/agent'
+import { components } from '../_generated/api'
 
 export const getOne = query({
   args: {
@@ -19,7 +21,17 @@ export const getOne = query({
     const conversation = await ctx.db.get(args.conversationId)
 
     if (!conversation) {
-      return null
+      throw new ConvexError({
+        code: 'NOT_FOUND',
+        message: 'Conversation Not Found',
+      })
+    }
+
+    if (conversation.contactSessionId !== session._id) {
+      throw new ConvexError({
+        code: 'UNAUTHORIZED',
+        message: 'Incorrect Session',
+      })
     }
 
     return {
@@ -44,8 +56,18 @@ export const create = mutation({
       })
     }
 
-    // TODO: Generate a unique thread ID
-    const threadId = '123'
+    const { threadId } = await supportAgent.createThread(ctx, {
+      userId: args.organizationId,
+    })
+
+    await saveMessage(ctx, components.agent, {
+      threadId: threadId,
+      message: {
+        role: "assistant",
+        // TODO: Later modify to widget settings initial message
+        content: "Hello! How can we assist you today?"
+      },
+    })
 
     const conversationId = await ctx.db.insert('conversations', {
       contactSessionId: session._id,

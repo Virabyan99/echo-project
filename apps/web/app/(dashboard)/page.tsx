25ddd22331e9b1d5039b4c43098f2b4eff08537a
Virabyan99 <gmparstone99@gmail.com>
2025-12-01 16:40:45 +0400

    35 deployements

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,12 +1,8 @@
 'use client'
-import { useMutation, useQuery } from 'convex/react'
-import { api } from '@workspace/backend/_generated/api'
-import { Button } from '@workspace/ui/components/button'
+
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 
 export default function Page() {
-  const users = useQuery(api.users.getMany)
-  const addUser = useMutation(api.users.add)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">

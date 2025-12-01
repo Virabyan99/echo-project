3aded939fe42b89e9f13f77b306e4a6614d13fea
Virabyan99 <gmparstone99@gmail.com>
2025-12-01 15:52:02 +0400

    34: Embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+    WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+    DEFAULT_ORG_ID: "org_369Se1xQtGKFJJ7UcQE83fxiv5V",
+    DEFAULT_POSITION: "bottom-right" as const,
+}
